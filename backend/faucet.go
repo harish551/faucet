6783ef9bf0200755e89a0bf8820ab7cdb7939a2d
akhilkumarpilli <akhilkumar7947@gmail.com>
2021-08-04 11:32:50 +0530

    Move limit to faucet file

--- backend/faucet.go
+++ backend/faucet.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"errors"
@@ -18,6 +20,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/tomasen/realip"
+	"golang.org/x/time/rate"
 )
 
 type ErrorResponse struct {
@@ -100,7 +103,7 @@ func main() {
 
 	getR := r.Methods(http.MethodGet).Subrouter()
 	getR.HandleFunc("/faucet/{address}", curlFaucetHandler).Methods(http.MethodGet)
-	getR.Use(Limit)
+	getR.Use(limit)
 
 	if err := http.ListenAndServe(publicUrl, r); err != nil {
 		log.Fatal("failed to start server", err)
@@ -271,4 +274,73 @@ func checkAndExecuteTxsHandler(address string, res http.ResponseWriter, request 
 	})
 
 	return
+}
+
+// Create a custom visitor struct which holds the rate limiter for each
+// visitor and the last time that the visitor was seen.
+type visitor struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
+// Change the the map to hold values of the type visitor.
+var visitors = make(map[string]*visitor)
+var mu sync.Mutex
+
+// Run a background goroutine to remove old entries from the visitors map.
+func init() {
+	go cleanupVisitors()
+}
+
+func getVisitor(ip string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	v, exists := visitors[ip]
+	if !exists {
+		rt := rate.Every(15 * time.Minute / 10)
+		limiter := rate.NewLimiter(rt, 1)
+		// Include the current time when creating a new visitor.
+		visitors[ip] = &visitor{limiter, time.Now()}
+		return limiter
+	}
+
+	// Update the last seen time for the visitor.
+	v.lastSeen = time.Now()
+	return v.limiter
+}
+
+// Every minute check the map for visitors that haven't been seen for
+// more than 3 minutes and delete the entries.
+func cleanupVisitors() {
+	for {
+		time.Sleep(time.Minute)
+
+		mu.Lock()
+		for ip, v := range visitors {
+			if time.Since(v.lastSeen) > 3*time.Minute {
+				delete(visitors, ip)
+			}
+		}
+		mu.Unlock()
+	}
+}
+
+func limit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		limiter := getVisitor(ip)
+		if limiter.Allow() == false {
+			http.Error(w, http.StatusText(429), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
